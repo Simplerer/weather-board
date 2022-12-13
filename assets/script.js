eee41f80ca127c5f77aba16b56f00bb4b9420f89
A Stone <simplererstone@gmail.com>
2022-12-12 21:58:46 -0500

    working logic

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,48 @@
-var log = function(word) {
-    console.log(word);
+var searchInput = document.querySelector("#city");
+var button = document.getElementById("button");
+
+
+
+var getWeather = function (lat, lon) {
+
+
+
 }
-log('eat a banana')
+
+var gatherCoords = function(event) {
+    event.preventDefault();
+    
+    var cityName = searchInput.value.trim();
+
+    var getCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=226011e8e963e4a2251a03649b5adc44'
+
+    fetch(getCoordinates)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+          console.log(data);
+          for (var i = 0; i < data.length; i++){
+              console.log(data[i].state);
+              getWeather()
+
+            }
+
+        })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
+
+button.addEventListener('click', gatherCoords);
+
+// API key = 226011e8e963e4a2251a03649b5adc44
+// ---------------------------------------------this is for getting lat and lon-------------------
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+
+// ------------------------------------------this is for weather -----------------------------------
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=226011e8e963e4a2251a03649b5adc44'
